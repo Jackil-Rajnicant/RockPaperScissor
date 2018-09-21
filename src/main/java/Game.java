f16ea85c77f71f2a91017b5e33296609b53fac10
jackil.rajnicant <jackil.rajnicant@itv.com>
2018-09-21 16:41:31 +0100

    test1 - commits

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,7 +3,7 @@ public class Game {
     Player playerOne;
     Player playerTwo;
 
-
+    //Contructor
     public Game(Player playerOne, Player playerTwo) {
 
         this.playerOne = playerOne;
@@ -11,6 +11,7 @@ public class Game {
 
     }
 
+    //Check which player won the game
     public String getResult() {
 
         if(playerOne.getOption().equals(playerTwo.getOption())) return "Draw";
